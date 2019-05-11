9bf267fdd27273377d9650cfe2b601b72388a095
anna <katharina.anna@gmail.com>
2019-05-12 00:50:43 +0200

    aaa

--- app/src/main/java/de/hacktival/burst/MainActivity.java
+++ app/src/main/java/de/hacktival/burst/MainActivity.java
@@ -3,11 +3,14 @@ package de.hacktival.burst;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,9 +36,12 @@ import de.hacktival.burst.utils.Network;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LocationManager locationMangaer=null;
+    private LocationListener locationListener=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
 
         JSONObject requestParams = new JSONObject();
@@ -68,6 +74,15 @@ public class MainActivity extends AppCompatActivity {
                 });
 
         Network.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return  ;
+        }
+        locationMangaer = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        locationMangaer.requestLocationUpdates(LocationManager
+                .GPS_PROVIDER, 5000, 10,locationListener);
 
         setContentView(R.layout.activity_main); //set the layout
         final TextView simpleTextView = (TextView) findViewById(R.id.location_text); //get the id for TextView
